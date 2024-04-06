5ddcc7e0c5227158ca208365d6c01ee2a687163e
TalexDreamSoul <59305952+TalexDreamSoul@users.noreply.github.com>
2024-04-06 12:51:34 +0800

    修复无法输入姓名bug

--- src/App.vue
+++ src/App.vue
@@ -23,15 +23,15 @@ useHead({
   ],
 })
 
-let user = ref(localStorage.getItem("user"))
-const name = ref(user || "")
+const user = ref(localStorage.getItem("user"))
+const name = ref(user.value || "")
 
 // @ts-ignore
-window.$name = name
+// window.$name = name
 
 function go() {
   user.value = name.value
-  localStorage.setItem("user", name.value)
+  localStorage.setItem("user", name.value!)
 }
 </script>
 
@@ -45,11 +45,11 @@ function go() {
 
     <div py-4 />
 
-    <TheInput v-model="name" placeholder="请输入游戏名..." autocomplete="false" @keydown.enter="go" />
+    <TheInput v-model="name" placeholder="请输入游戏名..." />
     <label class="hidden" for="input">你的用户名是？</label>
 
     <div>
-      <button m-3 text-sm btn :disabled="!name" @touchstart="go" click="go">
+      <button m-3 text-sm btn :disabled="!name" @click="go">
         确认
       </button>
     </div>
